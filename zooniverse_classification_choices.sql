d7dc2e40b636922d7956ab7fc21023d26c805337
Patrick Lorch <pdl@clevelandmetroparks.com>
2017-08-31 11:48:13 -0400

    Added back in HOWMANY so we can have one less view

--- zooniverse_classification_choices.sql
+++ zooniverse_classification_choices.sql
@@ -3,6 +3,7 @@
 SELECT *,
   jsonb_array_elements(jsonb_array_elements(annotations::jsonb)->'value')->>'choice' as choice,
   jsonb_array_elements(jsonb_array_elements(annotations::jsonb)->'value')->'answers' as answers,
+  jsonb_array_elements(jsonb_array_elements(annotations::jsonb)->'value')->'answers'->'HOWMANY' as number_of_animals,
 -- This is just to deal with the wierd use of subject_id as a key rather than a value
   jsonb_object_keys(subject_data::jsonb) as subject_id,
   replace(split_part(split_part(jsonb_each(subject_data::jsonb)::text,',"',2),'")',1),'""','"')::jsonb as subject_rest
