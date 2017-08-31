ac8a2b0c346e2130147a87f3288bc0211e4d11fd
Patrick Lorch <pdl@clevelandmetroparks.com>
2017-08-31 11:04:58 -0400

    Initial commit after moving out of clean_classification.sql

---
+++ zooniverse_classification_duplicate_remove.sql
@@ -0,0 +1,8 @@
+-- Name:  zooniverse_classification_nodups
+-- Description:  creates table without duplicates from one workflow and version classification table from zooniverse
+SELECT * FROM (
+  SELECT *,
+  	ROW_NUMBER() OVER(PARTITION BY subject_ids, user_name ORDER BY subject_ids asc) AS Row
+  FROM camera_trap.zooniverse_classification_report_raw2) dups
+  WHERE 
+    dups.workflow_id=1432 AND dups.workflow_version=478.99 AND dups.Row = 1
