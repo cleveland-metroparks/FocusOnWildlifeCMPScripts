da7e13dced7245cd81ddb29373b21c573b970ad6
Patrick Lorch <pdl@clevelandmetroparks.com>
2017-10-20 17:26:10 -0400

    works and you can switch how you deal with ties so between choosing first in alphbetical list with NOTHINGTHERE sorted to bottom, and randomly choosing among ties

--- zooniverse_classification_concensus.sql
+++ zooniverse_classification_concensus.sql
@@ -5,68 +5,40 @@
 -- Comment: table with concensus choices, fraction, and peilou score from the zooniverse_classification_choices view
 
 SELECT subject_ids,
-  choice,
+  choice_z,
   num_classifications,
   number_votes_for_species,
   frac_votes_for_species,
   pielou_evenness,
-  row_no,
-  max_tie_row_no,
-  tie_break
+  last_value(ties.tie_row_no) OVER (PARTITION BY subject_ids) AS max_tie_row_no
   FROM(SELECT subject_ids,
-    choice,
+    choice_z,
     num_classifications,
     number_votes_for_species,
     frac_votes_for_species,
     pielou_evenness,
-    row_no,
-    max_tie_row_no,
-    CASE WHEN max_tie_row_no = 1 THEN 1
-      WHEN max_tie_row_no > 1 AND row_no = 1 AND choice = 'NOTHINGTHERE' THEN 2
-      WHEN max_tie_row_no > 1 AND row_no = 1 AND choice != 'NOTHINGTHERE' THEN 1
-      ELSE 2
-      END as tie_break
+    row_number() OVER (PARTITION BY subject_ids) AS tie_row_no
     FROM(SELECT subject_ids,
-        choice,
-        num_classifications,
-        number_votes_for_species,
-        frac_votes_for_species,
-        pielou_evenness,
-        row_no,
-        last_value(ties.row_no) OVER (PARTITION BY subject_ids) as max_tie_row_no
+      choice_z,
+      num_classifications,
+      number_votes_for_species,
+      frac_votes_for_species,
+      CASE WHEN frac_votes_for_species = 1.0 THEN 0
+        WHEN frac_votes_for_species < 1.0 THEN -(sum(frac_votes_for_species*log(frac_votes_for_species)) OVER (PARTITION BY subject_ids))/log(num_classifications)
+      END as pielou_evenness
+      FROM (SELECT subject_ids,choice_z,number_votes_for_species,
+        sum(number_votes_for_species) over (PARTITION BY subject_ids) as num_classifications,
+        (number_votes_for_species)::numeric / sum(number_votes_for_species) over (PARTITION BY subject_ids) as frac_votes_for_species, 
+        RANK() OVER (PARTITION BY subject_ids ORDER BY number_votes_for_species DESC) AS rn
         FROM(SELECT subject_ids,
-          choice,
-          num_classifications,
-          number_votes_for_species,
-          frac_votes_for_species,
-          pielou_evenness,
-          row_number() OVER (PARTITION BY subject_ids) AS row_no
-          FROM(SELECT subject_ids,
-            choice,
-            num_classifications,
-            number_votes_for_species,
-            frac_votes_for_species,
-            CASE WHEN frac_votes_for_species = 1.0 THEN 0
-              WHEN frac_votes_for_species < 1.0 THEN -(sum(frac_votes_for_species*log(frac_votes_for_species)) OVER (PARTITION BY subject_ids))/log(num_classifications)
-            END as pielou_evenness
-            FROM (SELECT subject_ids,choice,number_votes_for_species,
-              sum(number_votes_for_species) over (PARTITION BY subject_ids) as num_classifications,
-              (number_votes_for_species)::numeric / sum(number_votes_for_species) over (PARTITION BY subject_ids) as frac_votes_for_species, 
-              RANK() OVER (PARTITION BY subject_ids ORDER BY number_votes_for_species DESC) AS rn
-              FROM(SELECT subject_ids,
-                choice,
-                count(choice) as number_votes_for_species
-                FROM camera_trap.zooniverse_classification_choices
-                GROUP BY subject_ids,choice) AS counted
-              ORDER BY subject_ids,choice) as ranked
-              WHERE ranked.rn=1  -- Choose top ranked choice only. Still has ties among rn=1 subject_ids. Comment out to see whole table to see if it is working
-            ORDER BY subject_ids) as evenness
-      ORDER BY subject_ids) as ties
-    ORDER BY subject_ids) as tb2
--- WHERE CASE WHEN tb2.max_row_no > 1 THEN
---         tb2.row_no = 2
---       ELSE
---         tb2.row_no = 1
---       END
-  ORDER BY subject_ids) as fin
-  WHERE fin.tie_break = 1
+          choice_z,
+          count(choice_z) as number_votes_for_species
+          FROM camera_trap.zooniverse_classification_choices
+          GROUP BY subject_ids,choice_z) AS counted
+--        ORDER BY subject_ids,choice_z) as ranked  -- This version sorts Nothing there choices to end and chooses first
+        ORDER BY subject_ids,random()) as ranked  -- This version sorts choices randomly and chooses first to give a random choice
+        WHERE ranked.rn=1  -- Choose top ranked choice_z only. Still has ties among rn=1 subject_ids. Comment out to see whole table to see if it is working
+      ORDER BY subject_ids) as evenness
+    ORDER BY subject_ids) as ties
+  WHERE ties.tie_row_no=1  -- Resolves ties by sorting Z_NOTHINGTHERE to the end of sets and choosing first one (so alphabtically, which prefers fox squirrels, for example). Random among not NOTHINGTHERE would be better.
+  ORDER BY subject_ids
